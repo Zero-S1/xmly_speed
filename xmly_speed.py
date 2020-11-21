3acacaeba1d7729ebb0bcb2c943203a61ec95860
加藤惠 <mashiro@88.com>
2020-11-21 10:55:24 +0800

    Update xmly_speed.py

--- xmly_speed.py
+++ xmly_speed.py
@@ -51,7 +51,7 @@ if "XMLY_SPEED_COOKIE" in os.environ:
 devices = []
 notify_time = 19                            # 通知时间,24小时制,默认19
 XMLY_ACCUMULATE_TIME = 1                    # 希望刷时长的,此处置1,默认打开;关闭置0
-UserAgent = "Mozilla/5.0 (Linux; Android 9; MIX 2 Build/PKQ1.190118.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045410 Mobile Safari/537.36 iting(main)/1.8.39/android_1 kdtUnion_iting/1.8.39"
+UserAgent = "Mozilla/5.0 (Linux; Android 9; MIX 2 Build/PKQ1.190118.001; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045410 Mobile Safari/537.36 iting(main)/1.8.42/android_1 kdtUnion_iting/1.8.42"
 # 非iOS设备的需要的自行修改,自己抓包 与cookie形式类似
 
 
